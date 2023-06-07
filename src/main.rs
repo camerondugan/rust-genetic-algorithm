913c78d2e7595a70360ba16f63c04498125ab249
Cameron Dugan <cameron.dugan@protonmail.com>
2023-06-07 09:18:20 -0400

    improvements to the code including async and more mutation types

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,108 @@
 use std::cmp::Ordering::Equal;
 use rand::Rng;
+use futures::future::join_all;
+
+#[derive(Clone)]
+pub struct Creature {
+    brain: Vec<Vec<f32>>,
+    weights: Vec<Vec<f32>>,
+    in_size: usize,
+    out_size: usize,
+    mutate_speed: f32,
+    fitness: f32,
+}
+
+impl Default for Creature {
+    fn default() -> Creature {
+        let mut rng = rand::thread_rng();
+        let b = vec![(0..2).map(|_| {rng.gen_range(-1.0..1.0)}).collect()];
+        let mut c = Creature {
+            brain: b,
+            weights: vec![],
+            in_size: 2,
+            out_size: 1,
+            mutate_speed: 3.0,
+            fitness: -9999.9999,
+        };
+        c.fix_weights();
+        c
+    }
+}
 
 fn main() {
     let mut rng = rand::thread_rng();
-
     let mut horde:Vec<Creature> = vec![];
-    let death_rate = 700; //per epoch
+    let death_rate = 0.7; //per epoch
+    let horde_size = 1000;
+    let death_amount = (death_rate*(horde_size as f32)) as i32; //per epoch
    
-    for _ in 0..1000 {
+    for _ in 0..horde_size {
         horde.push(Creature{in_size:2,out_size:1,..Default::default()});
     }
     
-    let mut count = 0;
-    while horde[0].fitness < -0.2 {
+    futures::executor::block_on(async {
+        let mut count = 0;
+        while horde[0].fitness < 100.0 {
 
-        for mut creature in &mut horde {
-            let mut fitness = 0.0;
-            let mut out = creature.feed(vec![0.0,0.0]);
-            fitness -= dist(out,vec![0.0]);
-            out = creature.feed(vec![1.0,0.0]);
-            fitness -= dist(out,vec![1.0]);
-            out = creature.feed(vec![0.0,1.0]);
-            fitness -= dist(out,vec![1.0]);
-            out = creature.feed(vec![1.0,1.0]);
-            fitness -= dist(out,vec![1.0]);
-            creature.fitness = fitness;
+            let mut example_input = vec![];
+            let mut example_output = vec![];
+
+            example_input.push(vec![1.0,0.0]);
+            example_input.push(vec![0.0,1.0]);
+            example_input.push(vec![1.0,1.0]);
+            example_input.push(vec![0.0,0.0]);
+
+            example_output.push(vec![0.0]);
+            example_output.push(vec![0.0]);
+            example_output.push(vec![1.0]);
+            example_output.push(vec![1.0]);
+
+            join_all(horde
+                     .iter_mut()
+                     .map(|x| x.test_fitness(&example_input, &example_output)))
+                .await;
+
+            // https://www.reddit.com/r/rust/comments/29kia3/no_ord_for_f32/
+            horde.sort_by(
+                |a, b| b.fitness.partial_cmp(&a.fitness).unwrap_or(Equal)
+            );
+
+            println!("brain size: {:?}", horde[0].brain_size());
+            println!("brain mutate speed: {:?}", horde[0].mutate_speed);
+            println!("brain: {:?}", horde[0].brain);
+            
+
+            println!("best for 0,0: {:?}", horde[0].feed(&vec![0.0,0.0]));
+            println!("best for 0,1: {:?}", horde[0].feed(&vec![0.0,1.0]));
+            println!("best for 1,0: {:?}", horde[0].feed(&vec![1.0,0.0]));
+            println!("best for 1,1: {:?}", horde[0].feed(&vec![1.0,1.0]));
+
+            for i in 0..10 {
+                println!("{}th fitness: {:?}", i,horde[i].fitness);
+            }
+
+
+            let mut species_culled = vec![];
+
+            let hlen = horde.len();
+            let dedicated_survivor = (hlen as f32*0.6) as usize;
+            for i in 0..death_amount as usize {
+                horde.insert(i,horde[rng.gen_range(i..i+dedicated_survivor)].new_variant());
+            }
+
+            for _ in 0..death_amount {
+                species_culled.push(horde.pop().unwrap());
+            }
+            // old method
+
+           count += 1;
+           println!("count: {}",count)
         }
 
-        // https://www.reddit.com/r/rust/comments/29kia3/no_ord_for_f32/
-        horde.sort_by(|a, b| b.fitness.partial_cmp(&a.fitness).unwrap_or(Equal));
-        
-        for i in 0..10 {
-            println!("fitness: {:?}", horde[i].fitness);
-        }
-        println!("brain length: {:?}", horde[0].brain.len());
-        println!("best for 0,0: {:?}", horde[0].feed(vec![0.0,0.0]));
-        println!("best for 0,1: {:?}", horde[0].feed(vec![0.0,1.0]));
-        println!("best for 1,0: {:?}", horde[0].feed(vec![1.0,0.0]));
-        println!("best for 1,1: {:?}", horde[0].feed(vec![1.0,1.0]));
-
-        let mut species_culled = vec![];
-
-        for _ in 0..death_rate {
-            species_culled.push(horde.pop().unwrap());
-        }
-        let hlen = horde.len();
-        for _ in 0..death_rate {
-            horde.push(horde[rng.gen_range(0..hlen/3)].new_variant());
-        }
-
-       // species_culled.sort_by_key(|s| s.species);
-
-       // for creature in species_culled {
-       //     let mut found = false;
-       //     for i in 0..hlen {
-       //         if horde[i].species == creature.species {
-       //             horde.push(horde[i].new_variant());
-       //             found = true;
-       //             break;
-       //         }
-       //     }
-       //     if !found {
-       //         horde.push(creature.new_variant());
-       //     }
-       // }
-        count += 1;
-        println!("count: {}",count)
-    }
-    //});
-    //handle.join().unwrap();
+    });
 }
 
-fn dist(v1:Vec<f32>, v2:Vec<f32>) -> f32 {
+fn dist(v1:&Vec<f32>, v2:&Vec<f32>) -> f32 {
     let mut distance = 0.0;
     for i in 0..v1.len() {
         distance += (v1[i]-v2[i]).abs();
@@ -80,7 +111,6 @@ fn dist(v1:Vec<f32>, v2:Vec<f32>) -> f32 {
 }
 
 fn convolution(layer1:Vec<f32>, weights:&Vec<f32>, layer2:&Vec<f32>) -> Vec<f32> {
-    //println!("Convolution: {:?} {:?}",layer1,layer2);
     let mut conv_output:Vec<f32> = Vec::with_capacity(layer2.len());
     
     let mut xi = 0;
@@ -91,45 +121,15 @@ fn convolution(layer1:Vec<f32>, weights:&Vec<f32>, layer2:&Vec<f32>) -> Vec<f32>
             sum += weights[xi*l1_len+i]*layer1[i];
         }
         conv_output.push(sum);
-        //println!("{:?}", conv_output);
         xi+=1;
     }
 
     conv_output
 }
 
-#[derive(Clone)]
-pub struct Creature {
-    brain: Vec<Vec<f32>>,
-    weights: Vec<Vec<f32>>,
-    in_size: usize,
-    out_size: usize,
-    mutate_speed: f32,
-    fitness: f32,
-    species: i8,
-}
-
-impl Default for Creature {
-    fn default() -> Creature {
-        let mut rng = rand::thread_rng();
-        let b = vec![(0..2).map(|_| {rng.gen_range(-1.0..1.0)}).collect()];
-        let mut c = Creature {
-            brain: b,
-            weights: vec![],//temp until overridden
-            in_size: 2,
-            out_size: 1,
-            mutate_speed: 4.0,
-            fitness: -9999.9999,
-            species: rng.gen_range(0..32),
-        };
-        c.new_weights();
-        c
-    }
-}
-
 impl Creature {
-    pub fn feed(&self, input:Vec<f32>) -> Vec<f32> {
-        let mut current_layer = input;
+    pub fn feed(&self, input:&Vec<f32>) -> Vec<f32> {
+        let mut current_layer = input.clone();
 
         for i in 0..self.brain.len() {
             current_layer = convolution(current_layer,&self.weights[i],&self.brain[i]);
@@ -142,16 +142,17 @@ impl Creature {
         let mut rng = rand::thread_rng();
         let mut variant = self.clone();
 
-        let number = rng.gen_range(0..100);
-        variant.mutate_node();
+        let number = rng.gen_range(0..80);
         match number {
-            1..=10 => variant.add_random_node(),
-            //11..=15 => variant.remove_random_node(),
-            98 => variant.add_random_layer(),
-            90..=97 => variant.new_weights(),
-            //99 => variant.remove_random_layer(),
+            1 => variant.add_random_node(),
+            2 => variant.remove_random_node(),
+            3 => variant.add_random_layer(),
+            4 => variant.remove_random_layer(),
+            5..=20 => variant.random_weights(),
             _ => variant.mutate_node(),
         }
+        let change_more = rng.gen_range(0..1) != 0;
+        if change_more { variant = variant.new_variant();}
         variant
     }
 
@@ -163,66 +164,103 @@ impl Creature {
         size
     }
 
+    pub async fn test_fitness(&mut self,input:&Vec<Vec<f32>>, expected:&Vec<Vec<f32>>) {
+        let mut fitness = 0.0;
+        for tup in input.iter().zip(expected) {
+            let (inp, exp) = tup;
+            fitness -= dist(&self.feed(inp),&exp)
+        }
+        self.fitness = fitness;
+    }
+
     fn mutate_node(&mut self) {
         let mut rng = rand::thread_rng();
-        let layer_i = rng.gen_range(0..self.brain.len());
+        let mut layer_i = rng.gen_range(0..self.brain.len());
+        let start_layer = layer_i;
+        while self.brain[layer_i].len() == 0 {
+            layer_i+=1;
+            layer_i%=self.brain.len();
+            if layer_i == start_layer {return;}//infinite loop escape
+        }
         let node_i = rng.gen_range(0..self.brain[layer_i].len());
-        let modify_brain = rng.gen_range(0.0..1.0) >= 0.5;
+        let modify_brain = rng.gen_range(0..=1) == 0;
         let mut nodes = &self.brain;
         if !modify_brain {
             nodes = &self.weights;
         }
         let mut node = nodes[layer_i][node_i];
-        if rng.gen_range(0.0..1.0) >= 0.5 {
-            node += node * rng.gen_range(0.0..self.mutate_speed);
-        } else {
-            node -= node * rng.gen_range(0.0..self.mutate_speed);
+        node += rng.gen_range(-self.mutate_speed..self.mutate_speed);
+        let change_more = rng.gen_range(0..4) != 0;
+        if change_more {
+            self.mutate_node();
         }
         self.brain[layer_i][node_i] = node;
-        let change_more = rng.gen_range(0.0..1.0) >= 0.05;
-        if change_more { self.mutate_node();}
     }
 
     fn add_random_layer(&mut self) {
         let mut rng = rand::thread_rng();
-        self.brain.push((0..rng.gen_range(1..10)).map(|_| {rng.gen_range(-1.0..1.0)}).collect());
-        self.new_weights();
-    }
-
-    fn remove_random_layer(&mut self) {
-        if self.brain.len() > 1 {
-            self.brain.pop();
-        }
-        self.new_weights();
+        self.brain.push((0..rng.gen_range(2..4)).map(|_| {rng.gen_range(-1.0..1.0)}).collect());
+        self.fix_weights();
     }
 
     fn add_random_node(&mut self) {
         let mut rng = rand::thread_rng();
+        let new_node = rng.gen_range(-1.0..1.0);
         let layer_i = rng.gen_range(0..self.brain.len());
-        let node_i = rng.gen_range(0..self.brain[layer_i].len());
-        self.brain[layer_i].insert(
-            node_i,rng.gen_range(-self.mutate_speed..self.mutate_speed)
-        );
-        let change_more = rng.gen_range(0.0..1.0) >= 0.1;
-        if change_more { self.add_random_node();}
-        self.new_weights();
+        if self.brain[layer_i].len() == 0 {
+            self.brain[layer_i].push(new_node);
+        } else {
+            let node_i = rng.gen_range(0..self.brain[layer_i].len());
+            self.brain[layer_i].insert(node_i, new_node);
+
+        }
+        self.fix_weights();
     }
 
     fn remove_random_node(&mut self) {
-
+        if self.brain_size() <= 3 {return;}
+        let mut rng = rand::thread_rng();
+        if self.brain.len() <= 0 {return};
+        let layer_i = rng.gen_range(0..self.brain.len());
+        if self.brain[layer_i].len() <= 2 {return};
+        let node_i = rng.gen_range(0..self.brain[layer_i].len());
+        self.brain[layer_i].remove(node_i);
+        self.fix_weights();
     }
 
-    fn new_weights(&mut self) {
+    fn remove_random_layer(&mut self) {
+        if self.brain_size() <= 3 {return;}
         let mut rng = rand::thread_rng();
-        let mut w: Vec<Vec<f32>> = Vec::with_capacity(self.brain.len());
-        let mut last_len = self.in_size;
-        for i in 0..self.brain.len() {
-            w.push((0..last_len*self.brain[i].len()).map(|_| 
-                {
-                    rng.gen_range(-1.0..1.0)
-                }).collect());
-            last_len = self.brain[i].len()
+        if self.brain.len() <= 1 {return};
+        let layer_i = rng.gen_range(0..self.brain.len());
+        self.brain.remove(layer_i);
+        self.weights.remove(layer_i);
+        self.fix_weights();
+    }
+
+
+    fn random_weights(&mut self) {
+        self.weights.clear();
+        self.fix_weights();
+    }
+
+    fn fix_weights(&mut self) {
+        let mut rng = rand::thread_rng();
+        //let mut w: Vec<Vec<f32>> = vec![];
+        let mut last_len: usize = self.in_size;
+        if self.weights.len() < self.brain.len() { //missing layers
+            for _ in self.weights.len()..self.brain.len() {
+                self.weights.push(vec![]); //add empty layers
+            }
         }
-        self.weights = w;
+        for i in 0..self.brain.len() { // missing nodes
+                if self.brain[i].len() == 0 {continue;};
+                let mut weights : Vec<f32> = (self.weights[i].len()..last_len*self.brain[i].len()).map(|_| 
+                rng.gen_range(-self.mutate_speed..self.mutate_speed)).collect();
+                self.weights[i].append(&mut weights);
+                last_len = self.brain[i].len();
+        }
+
+        //rng.gen_range(-self.mutate_speed..self.mutate_speed)).collect());
     }
 }
